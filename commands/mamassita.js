15da2f506ef75d6e3db8689323a663bae744419f
fakhreddine jalleli <fakhrijalleli@gmail.com>
2018-11-24 19:39:42 +0100

    Delete mamassita.js

--- commands/mamassita.js
+++
@@ -1,9 +0,0 @@
-const Discord =  require('discord.js');
-const Attachment  = require('discord.js');
-const client = new Discord.Client();
-
-exports.run = async (client, message, args, ops) => {
-    const attachment = new Discord.Attachment('D:/mamsita.png');
-       
-        message.channel.send(attachment);
-    };
